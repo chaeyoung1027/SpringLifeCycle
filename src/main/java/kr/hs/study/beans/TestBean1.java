6a645415be37fbb58b6f683469261d0127bb67ed
Chaeyoung Lim <rnldyal1027@naver.com>
2023-03-16 14:33:16 +0900

    기본생성자에 출력문 추가

--- src/main/java/kr/hs/study/beans/TestBean1.java
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -1,7 +1,9 @@
 package kr.hs.study.beans;
 
 public class TestBean1 {
-    TestBean1(){}
+    TestBean1(){
+        System.out.println("TestBean1의 기본생성자");
+    }
     public void init_method() {
         System.out.println("TestBean1의 init method");
     }
