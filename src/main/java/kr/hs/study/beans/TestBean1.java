5ee4c3d474ea401b8a7b057dfe899bf622ce9f85
Chaeyoung Lim <rnldyal1027@naver.com>
2023-03-16 14:42:11 +0900

    init, destroy method 이해!

--- src/main/java/kr/hs/study/beans/TestBean1.java
+++ src/main/java/kr/hs/study/beans/TestBean1.java
@@ -4,10 +4,10 @@ public class TestBean1 {
     TestBean1(){
         System.out.println("TestBean1의 기본생성자");
     }
-    public void init_method() {
+    public void init_method() {     //생성자 실행 후에 자동으로 호출되어 실행
         System.out.println("TestBean1의 init method");
     }
-    public void destroy_method(){
+    public void destroy_method(){   //객체가 소멸일때 자동으로 호출
         System.out.println("TestBean1의 destroy method");
     }
 
